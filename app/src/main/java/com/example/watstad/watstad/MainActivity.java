244708a5c02e1cd8e007132352be05555e929b64
Dylano <dylano.hartman@gmail.com>
2018-10-05 15:23:58 +0200

    Added Fragment Navigation Functionality

--- app/src/main/java/com/example/watstad/watstad/MainActivity.java
+++ app/src/main/java/com/example/watstad/watstad/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.watstad.watstad;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener );
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFregment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_profile:
+                            selectedFregment = new ProfileFragment();
+                            break;
+                        case R.id.nav_home:
+                            selectedFregment = new HomeFragment();
+                            break;
+                        case R.id.nav_recent:
+                            selectedFregment = new RecentFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFregment).commit();
+
+                    return true;
+                }
+            };
 }
